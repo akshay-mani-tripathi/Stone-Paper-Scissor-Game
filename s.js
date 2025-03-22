b28d38979fd7a2a5604f9d4cb61795e70c166a35
Akshay mani tripathi <152083530+akshay-mani-tripathi@users.noreply.github.com>
2025-03-22 20:00:05 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,32 @@
+let first = document.querySelectorAll(".box");
+const arr = ["Stone", "Paper", "Scissor"];
+let stopp = document.querySelector("#stop");
+let second = document.querySelector("#start");
+let pa = document.querySelector("#pa");
+let tr = false;
+
+second.addEventListener("click", () => {
+    pa.innerText = "Game Started";
+    console.log("Game Started");
+    tr = true;
+});
+
+stopp.addEventListener("click", () => {
+    pa.innerText = "Game Stopped";
+    console.log("Game Stopped, click start to play again");
+    tr = false;
+});
+
+first.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (!tr) return;
+        let nu = Math.floor(Math.random() * 3);
+        if (box.innerText === arr[nu]) {
+            pa.innerText = "You Won!";
+            console.log("You Won!");
+        } else {
+            pa.innerText = "Try again, Answer was " + arr[nu];
+            console.log("You Lose! Answer was", arr[nu]);
+        }
+    });
+});
